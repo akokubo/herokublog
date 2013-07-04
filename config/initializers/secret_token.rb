12e7b679539bccd196bd652358f847d0fcb5a19d
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-07-04 18:08:51 +0900

    アプリ初期化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Herokublog::Application.config.secret_key_base = '71571d723abc01a8ed65347bc3ba1ecf05e8048cafb6bd2c96ded61b1185b47e9f10cd7fe4c54a123930cacb2d0eaf24e26e560922cf32fea09df06a5d0c824c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Herokublog::Application.config.session_store :cookie_store, key: '_herokublog_session'
